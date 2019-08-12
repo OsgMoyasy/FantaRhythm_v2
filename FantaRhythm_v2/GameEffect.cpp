14f9b24ba6a8396f04c42300d5a177102725e75c
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-08-12 16:48:37 +0900

    SEクラスとmapchipクラスを追加

--- FantaRhythm_v2/GameEffect.cpp
+++ FantaRhythm_v2/GameEffect.cpp
@@ -1,5 +1,6 @@
 #include "GameEffect.h"
 
+/*
 void GameEffect::playSE(SE::NAME name, SE::TYPE type) {
 	static Audio* hit = new Audio(U"resources/musics/effects/decision.mp3");//Žb’è‰¹ºƒtƒ@ƒCƒ‹
 
@@ -35,4 +36,42 @@ void GameEffect::playME(ME::NAME name) {
 		playSE(SE::HIT, SE::REP);
 		playVE(VE::HIT);
 	}
+}
+*/
+
+SE::SE(FilePath& path) {
+	sound = new Audio(path);
+}
+SE::~SE() {
+	delete sound;
+}
+void SE::play() {
+	sound->stop();
+	sound->play();
+}
+
+
+mapchip::mapchip(FilePath& path, int yChipHeight, int xChipWidth) {
+	map = new Texture(path);
+	this->yChipHeight = yChipHeight;
+	this->xChipWidth = xChipWidth;
+	yMapHeight = map->height();
+	xMapWidth = map->width();
+}
+mapchip::~mapchip() {
+	delete map;
+}
+TextureRegion mapchip::chipFromMap() {
+	static int nowPosX = 0;
+	static int nowPosY = 0;
+	TextureRegion chip = (*map)(nowPosX, nowPosY, xChipWidth, yChipHeight);
+
+	if (nowPosX += xChipWidth, nowPosX >= xMapWidth) {
+		nowPosX = 0;
+		if (nowPosY += yChipHeight, nowPosY >= yMapHeight) {
+			nowPosY = 0;
+		}
+	}
+
+	return chip;
 }
--- FantaRhythm_v2/GameEffect.h
+++ FantaRhythm_v2/GameEffect.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "FantaRhythm_v2.h"
 
+/*
 namespace SE {
 	enum NAME {
 		HIT
@@ -21,9 +22,31 @@ namespace ME {
 		HIT
 	};
 }
+
 class GameEffect {
 public:
 	static void playSE(SE::NAME name, SE::TYPE type);	//効果音
 	static void playVE(VE::NAME name);	//視覚演出
 	static void playME(ME::NAME name);	//効果音と視覚演出のセット
+};
+*/
+
+struct SE {
+	SE(FilePath& path);
+	~SE();
+	void play();
+private:
+	Audio* sound;
+};
+
+struct mapchip {	//渡されたマップチップ画像からチップ画像を切り出す
+	mapchip(FilePath& path, int yChipHeight, int xChipWidth);
+	~mapchip();
+	TextureRegion chipFromMap();	//呼ばれる度に次の位置のチップ画像のポインタを返す
+private:
+	Texture* map;
+	int yChipHeight;
+	int xChipWidth;
+	int yMapHeight;
+	int xMapWidth;
 };
