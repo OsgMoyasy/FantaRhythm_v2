376b7c772048c120b7773b3ff3d47cff2ba8246b
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-08-14 15:09:33 +0900

    mapflipクラスとflipMovieクラスを、変数がXとYの並びになるように整形 fractalクラスを追加

--- FantaRhythm_v2/GameEffect.h
+++ FantaRhythm_v2/GameEffect.h
@@ -36,31 +36,41 @@ class SE {
 public:
 	SE(FilePath& path);
 	~SE();
-	void play();
+	void play();//SEを再生する。重複して再生されようとした時、最初から再生しなおす
 };
 
 class mapflip {	//渡されたマップチップ画像からチップ画像を切り出す
-	Texture* map;
-	int yFlipHeight;
+	Texture* flip;
 	int xFlipWidth;
-	int yMapHeight;
+	int yFlipHeight;
 	int xMapWidth;
+	int yMapHeight;
 	int nowPosX;
 	int nowPosY;
 public:
-	mapflip(const FilePath& path, int yFlipHeight, int xFlipWidth);
+	mapflip(const FilePath& path, int xFlipWidth, int yFlipHeight);
 	~mapflip();
-	bool mapflip::nextFlip();
-	TextureRegion* flipFromMap();	//呼ばれる度に次の位置のチップ画像のポインタを返す
-	inline TextureRegion* operator()() { return flipFromMap(); }
+	bool mapflip::nextFlip();	//次の位置のチップ画像をセット。次がない(最後)ならfalseを返す
+	TextureRegion getFlip();	//チップ画像を返す
 };
 
-class flipMovie :IEffect {
+struct flipMovie :IEffect {//mapflipを利用してパラパラ漫画を作るエフェクト
+	flipMovie(const FilePath& path, int xFlipWidth, int yFlipHeight, int drawX, int drawY);
+	~flipMovie();
+	bool update(double t);
+private:
 	mapflip* flip;
 	int drawX;
 	int drawY;
-public:
-	flipMovie(const FilePath& path, int yFlipHeight, int xFlipWidth, int drawX, int drawY);
-	~flipMovie();
-	bool update(double t);
 };
+
+struct fractal :IEffect {//フラクタル模様を作り出すエフェクト
+	int firstX, firstY;
+	int firstSize;
+	int x2, y2;
+	fractal(int size, int x, int y);
+	bool update(double t);
+private:
+	void drawTriangle(double t, int posX, int posY, int size, int x2, int y2);
+};
+
