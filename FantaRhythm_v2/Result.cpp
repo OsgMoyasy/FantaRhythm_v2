7ffe18a7550c7e9073b31365c9bc426623312be9
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-08-15 21:31:19 +0900

    過去問追加

--- FantaRhythm_v2/Result.cpp
+++ FantaRhythm_v2/Result.cpp
@@ -5,13 +5,34 @@ Result::Result(JUDGE::JudgeCount* judgecnt, int totaldmg, bool cflag) {
 	judgecount = judgecnt;
 	totaldamage = totaldmg;
 	clearflag = cflag;
+
+
+	if (cflag) {//ゲームクリア用
+	}
+	else {//ゲームオーバー用
+	}
 }
 Result::~Result(void) {
-
+	TextureAsset::UnregisterAll();
 }
 void Result::update(void) {
 
 }
 void Result::draw(void) {
+
+}
+//ゲームクリア用
+void Result::gameclearUpdate(void) {
+
+}
+
+void Result::gameclearDraw(void) {
+
+}
+//ゲームオーバー用
+void Result::gameoverUpdate(void) {
+
+}
+void Result::gameoverDraw(void) {
 
 }
