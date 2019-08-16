866a5163693d067e7ee0a9920f6d07ed09207382
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-08-16 18:04:36 +0900

    コメントアウトしていた以前のソースコードを削除 アクセス修飾子の設定の誤りを修正&整形

--- FantaRhythm_v2/GameEffect.cpp
+++ FantaRhythm_v2/GameEffect.cpp
@@ -1,44 +1,5 @@
 #include "GameEffect.h"
 
-/*
-void GameEffect::playSE(SE::NAME name, SE::TYPE type) {
-	static Audio* hit = new Audio(U"resources/musics/effects/decision.mp3");//暫定音声ファイル
-
-	Audio* sound = NULL;
-	switch (name) {
-	case SE::HIT:
-		sound = hit;
-		break;
-	}
-
-	switch (type) {
-	case SE::DUL:
-		sound->playOneShot();
-		break;
-	case SE::REP:
-		sound->stop();
-		sound->stopAllShots();
-		sound->play();
-		break;
-	case SE::IGN:
-		sound->play();
-		break;
-	}
-}
-
-void GameEffect::playVE(VE::NAME name) {
-	//未実装(Siv3DのEffectクラスを使うつもり)
-}
-
-void GameEffect::playME(ME::NAME name) {
-	switch (name) {
-	case ME::HIT:
-		playSE(SE::HIT, SE::REP);
-		playVE(VE::HIT);
-	}
-}
-*/
-
 SE::SE(FilePath& path) {
 	sound = new Audio(path);
 }
--- FantaRhythm_v2/GameEffect.h
+++ FantaRhythm_v2/GameEffect.h
@@ -1,45 +1,22 @@
 #pragma once
 #include "FantaRhythm_v2.h"
 
-/*
-namespace SE {
-	enum NAME {
-		HIT
-	};
-	enum TYPE {//同じSEが複数再生された時の処理
-		DUL,	//重複
-		REP,	//最初から再生
-		IGN		//無視
-	};
-}
-namespace VE {
-	enum NAME {
-		HIT
-	};
-}
-namespace ME {
-	enum NAME {
-		HIT
-	};
-}
-
-class GameEffect {
-public:
-	static void playSE(SE::NAME name, SE::TYPE type);	//効果音
-	static void playVE(VE::NAME name);	//視覚演出
-	static void playME(ME::NAME name);	//効果音と視覚演出のセット
-};
-*/
-
 class SE {
-	Audio* sound;
 public:
 	SE(FilePath& path);
 	~SE();
 	void play();//SEを再生する。重複して再生されようとした時、最初から再生しなおす
+private:
+	Audio* sound;
 };
 
 class mapflip {	//渡されたマップチップ画像からチップ画像を切り出す
+public:
+	mapflip(const FilePath& path, int xFlipWidth, int yFlipHeight);
+	~mapflip();
+	bool nextFlip();	//次の位置のチップ画像をセット。次がない(最後)ならfalseを返す
+	TextureRegion getFlip();	//チップ画像を返す
+private:
 	Texture* flip;
 	int xFlipWidth;
 	int yFlipHeight;
@@ -47,11 +24,6 @@ class mapflip {	//渡されたマップチップ画像からチップ画像を切り出す
 	int yMapHeight;
 	int nowPosX;
 	int nowPosY;
-public:
-	mapflip(const FilePath& path, int xFlipWidth, int yFlipHeight);
-	~mapflip();
-	bool mapflip::nextFlip();	//次の位置のチップ画像をセット。次がない(最後)ならfalseを返す
-	TextureRegion getFlip();	//チップ画像を返す
 };
 
 struct flipMovie :IEffect {//mapflipを利用してパラパラ漫画を作るエフェクト
@@ -65,12 +37,13 @@ private:
 };
 
 struct fractal :IEffect {//フラクタル模様を作り出すエフェクト
-	int firstX, firstY;
-	int firstSize;
-	int x2, y2;
 	fractal(int size, int x, int y);
 	bool update(double t);
 private:
+	int firstX, firstY;
+	int firstSize;
+	int x2, y2;
+
 	void drawTriangle(double t, int posX, int posY, int size, int x2, int y2);
 };
 
