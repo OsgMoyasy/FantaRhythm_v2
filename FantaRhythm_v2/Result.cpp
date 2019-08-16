9c0d0a2fda2c4f7c081aa3cbb1108e04976c9dac
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-08-16 14:59:43 +0900

    ゲームオーバー画面を追加

--- FantaRhythm_v2/Result.cpp
+++ FantaRhythm_v2/Result.cpp
@@ -5,13 +5,52 @@ Result::Result(JUDGE::JudgeCount* judgecnt, int totaldmg, bool cflag) {
 	judgecount = judgecnt;
 	totaldamage = totaldmg;
 	clearflag = cflag;
+	FontAsset::Register(U"font", 50);
+	FontAsset::Preload(U"font");
+	if (clearflag) {//ゲームクリア
+		TextureAsset::Register(U"back", U"resources/images/back/BackScreen.jpg");
+		stateUpdate = &Result::successUpdate;
+		stateDraw = &Result::successDraw;
+	}
+	else {//ゲームオーバー
+		TextureAsset::Register(U"back", U"resources/images/back/gameOver.jpg");
+		stateUpdate = &Result::failedUpdate;
+		stateDraw = &Result::failedDraw;
+
+		framecnt = 0;
+	}
+	TextureAsset::Preload(U"back");//背景のロード
 }
 Result::~Result(void) {
-
+	TextureAsset::UnregisterAll();
 }
 void Result::update(void) {
-
+	(this->*stateUpdate)();
 }
 void Result::draw(void) {
+	
 
+	(this->*stateDraw)();
+}
+
+//ゲームクリア用
+void Result::calcScore(void) {//スコア計算
+	constexpr int weight[JUDGE::TYPE_SIZE] = {100, 70, 50, 0};
+	//score = 
+}
+void Result::successUpdate(void) {
+
+}
+void Result::successDraw(void) {
+	TextureAsset(U"back").drawAt(Window::Width() / 2, Window::Height() / 2);
+}
+//ゲームオーバー用
+void Result::failedUpdate(void) {
+	if (framecnt <= alphatime) {
+		alpha = (double)framecnt / alphatime * 0.6;
+		framecnt++;
+	}
+}
+void Result::failedDraw(void) {
+	TextureAsset(U"back").drawAt(Window::Width() / 2, Window::Height() / 2, AlphaF(alpha));//背景描画
 }
--- FantaRhythm_v2/Result.h
+++ FantaRhythm_v2/Result.h
@@ -3,6 +3,8 @@
 #include "FantaRhythm_v2.h"
 #include "Scene.h"
 
+
+
 namespace JUDGE {
 	union _JudgeCount;
 	typedef _JudgeCount JudgeCount;
@@ -14,8 +16,27 @@ public:
 	~Result(void);
 	void update(void);
 	void draw(void);
+
 private:
 	int totaldamage;
-	JUDGE::JudgeCount *judgecount;
+	JUDGE::JudgeCount* judgecount;
 	bool clearflag;
+
+	int framecnt;
+
+	void(Result::* stateUpdate)(void);//実行する計算処理への関数ポインタ
+	void(Result::* stateDraw)(void);//実行する描画処理への関数ポインタ
+
+	//ゲームクリア用
+	int score;
+
+
+	void calcScore(void);//スコア計算
+	void successUpdate(void);
+	void successDraw(void);
+	//ゲームオーバー用
+	static const int alphatime = 4 * 60;//アルファ値が元に戻るまでの時間×フレーム数
+	double alpha;
+	void failedUpdate(void);
+	void failedDraw(void);
 };
