04f8750da27019d8adc9449c05bf1fc7b10f3754
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-11-03 13:54:24 +0900

    [add]タイトルから説明書を見られるようにした

--- FantaRhythm_v2/Title.cpp
+++ FantaRhythm_v2/Title.cpp
@@ -11,6 +11,8 @@ Title::Title(void) {
 	FontAsset::Register(U"titlefont", 50);
 	FontAsset::Preload(U"titlefont");
 	TextureAsset::Register(U"titleback", U"resources/images/back/start_back_image.jpg", AssetParameter::LoadAsync());
+	tutorial_img= Texture(U"resources/images/items/fanta_kaisetu.png");
+	tutorialF = false;
 }
 
 Title::~Title(void) {
@@ -35,11 +37,18 @@ void Title::start(void) {
 
 
 void Title::update(void) {
+	static bool pressed = false;
 	if (MyKey::getDecisionKey()) {
 		OperationSE::play(DECISION);
 		SceneManager::setNextScene(SceneManager::SCENE_QRREAD);
 	}
+	else if (MyKey::getReturnKey()) {
+		pressed = true;
+		OperationSE::play(DECISION);
+		tutorialF = !tutorialF;
+	}
 	else {
+		pressed = false;
 		changeAlpha();
 	}
 }
@@ -52,10 +61,20 @@ void Title::draw(void) {
 	FontAsset(U"titlefont")(U"Å`Press Button To StartÅ`").drawAt(Window::Width() / 2 + 3, Window::Height() - 150 + 3, ColorF(0, 0, 0, alpha - 0.05));
 	FontAsset(U"titlefont")(U"Å`Press Button To StartÅ`").drawAt(Window::Width() / 2, Window::Height() - 150, AlphaF(alpha));
 
-	OperationTexture::draw(DECISION, { 10,630 }, U"ÅFêiÇﬁ");
+	OperationTexture::draw(DECISION, { 10,580 }, U"ÅFêiÇﬁ");
+	OperationTexture::draw(CANCEL, { 10,630 }, U"ÅFëÄçÏê‡ñæ");
+
+	if (tutorialF) {
+		drawTutorial();
+	}
 }
 
 void Title::changeAlpha(void) {
 	const uint64 t = Time::GetMillisec();
 	alpha = Sin(t % CYCLE / static_cast<double>(CYCLE) * TWOPI) * 0.42 + 0.58;
+}
+
+void Title::drawTutorial(void) {
+	double hi = (double)Window::Width() / tutorial_img.width();
+	tutorial_img.scaled(hi).draw(0, 0);
 }
